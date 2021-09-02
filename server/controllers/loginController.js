b2edfb3722440ce6782424bb019c515cf2d7496b
NourAdawi <nouradwi12@gmail.com>
2021-09-02 13:51:19 +0300

    update file strucutre

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const emailExists = require('../postgres/query/emailExists');
 const { loginValidation } = require('../utils/validation');
 
-const routerlogin = express.Router();
-
-routerlogin.post('/login', async (req, res) => {
-  // validate data before we make a user
+const routerlogin = async (req, res) => {
+  // validate data before we login in
   const { error } = await loginValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-});
+};
 
 module.exports = routerlogin;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 const registerRouter = require('../controllers/registerController');
-// const loginRouter = require('../controllers/loginController');
+const loginRouter = require('../controllers/loginController');
 
 router.post('/register', registerRouter);
-// router.post('/login', loginRouter);
+router.post('/login', loginRouter);
 
 module.exports = router;
